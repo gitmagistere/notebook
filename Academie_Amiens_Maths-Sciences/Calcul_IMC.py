c678b6b65c9cc0922a5204a5b24989626ffe4624
Pataillee <57292240+Pataillee@users.noreply.github.com>
2019-11-02 15:43:07 +0100

    Add files via upload

---
+++ Academie_Amiens_Maths-Sciences/Calcul_IMC.py
@@ -0,0 +1,9 @@
+﻿from lycee import *
+from math import *
+
+m=demande("Masse en kilogramme : ")
+t=demande("Taille en mètre : ")
+
+IMC=m/(t*t)
+
+print ("L'IMC est de : " ,IMC)
---
+++ Academie_Amiens_Maths-Sciences/Table_multiplication.py
@@ -0,0 +1,7 @@
+﻿from lycee import *
+from math import *
+
+table=1 #On peut insérer une demande pour choisir la table.
+
+for i in range(0,11): #Pour écrire la table de 0 jusque 10, le 11 est exclu.
+    print(table,"x",i,"=",table*i)
