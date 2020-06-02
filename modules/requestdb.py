234de3b4afd6821417d514c5e8fc5e0b0e48e96a
Laurent Signac <laurent.signac@univ-poitiers.fr>
2020-06-02 13:44:06 +0200

    Modif module SQL

--- modules/requestdb.py
+++ modules/requestdb.py
@@ -3,39 +3,53 @@ import os.path
 # import pandas # cf pretty_print
 from IPython.display import display, HTML
 
+__version__ = (1,1)
+##  Règle le pb des noms de colonnes identiques
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        basename = col[0]
+        name = basename
+        k = 2
+        while name in d:
+            name = "{}({:1d})".format(basename, k)
+            k += 1
+        d[name] = row[idx]
+    return d
+
 class RequestDB():
-    
+    LIMIT_OUTPUT_ = 100
+
     def __init__(self, databasename):
         if not os.path.isfile(databasename):
             print("No such file : {}".format(databasename))
             self.db = None
         else:
             self.db = databasename
-    
+
     def _make_a_request(self, request, params=None):
         if self.db is None:
             print("No database")
             return None
-        cnx = sqlite3.connect(self.db)
-        cnx.row_factory = sqlite3.Row
-        c = cnx.cursor()
-        try:
-            if params is None:
-                c.execute(request)
-            else:
-                c.execute(request, params)
-        except sqlite3.OperationalError as e:
-            print("OperationalError : {}".format(e))
-            return None
-        data = [dict(row) for row in c.fetchall()]
-        cnx.close()
+        with sqlite3.connect(self.db) as cnx:
+            cnx.row_factory = dict_factory
+            c = cnx.cursor()
+            try:
+                if params is None:
+                    c.execute(request)
+                else:
+                    c.execute(request, params)
+            except sqlite3.OperationalError as e:
+                print("OperationalError : {}".format(e))
+                return None
+            data = [dict(row) for row in c.fetchall()]
         return data
 
     @staticmethod
     def _prettyprint_table_with_pandas(data):
         df = pandas.DataFrame(data)
         return df.to_html()
-    
+
     @staticmethod
     def _prettyprint_table(data):
         if not data:
@@ -50,21 +64,21 @@ class RequestDB():
             lines.append("</tr>")
         lines.append("</tbody></table>")
         return "\n".join(lines)
-            
 
-    def query(self, request, size=20):
+
+    def query(self, request, *, force=False):
         res = self._make_a_request(request)
         if res is None or len(res) == 0:
             print("No results")
         else:
             header = "<p>Number of rows : <b>{}</b></p>".format(len(res))
-            if len(res) <= size:
+            if len(res) <= self.LIMIT_OUTPUT_ or force:
                 return HTML(header + self._prettyprint_table(res))
             else:
-                header += "<p><b>First lines only...</b></p>"
-                footer = "<p>...</p>You can try : query(&lt;your_query&gt;, size={}) if you know what you are doing".format(len(res))
-                return HTML(header + self._prettyprint_table(res[:size]) + footer)
-        
+                header += "<p><b>Output too large...</b></p>"
+                footer = "<p>You can force output with query(&lt;your_query&gt;, force=True) if you know what you are doing</p>"
+                return HTML(header + footer)
+
     def infos(self):
         lines = []
         res  = self._make_a_request("SELECT name FROM sqlite_master WHERE type='table';")
@@ -75,4 +89,3 @@ class RequestDB():
             resf = [{'name': _["name"], "type": _['type'], 'notnull': _['notnull']} for _ in res]
             lines.append(self._prettyprint_table(resf))
         return HTML("\n".join(lines))
-
