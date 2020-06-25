4590e2152a58e6b9fcc1ab7de29ecf84097d5489
gitmagistere <48481569+gitmagistere@users.noreply.github.com>
2020-06-25 16:41:39 +0200

    Ajout du fichier « Site_Pg1Dimensions.py » du Jupyter Notebook « Manipuler des images en Python » dans le parcours «  Formations thématiques SNT [Prc Toulouse janvier 2020] ».

---
+++ ac-toulouse/9762/Site_Pg1Dimensions.py
@@ -0,0 +1,6 @@
+﻿from PIL import Image
+img = Image.open("sernin.jpg")
+largeur, hauteur = img.size
+print("largeur de l'image :", largeur, "pixels")
+print("hauteur de l'image :", hauteur, "pixels")
+
