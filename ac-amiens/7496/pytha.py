ad935eab4a2fc996a1ba5fbce48a120abab24d8b
gitmagistere <48481569+gitmagistere@users.noreply.github.com>
2020-11-06 18:29:58 +0100

    Ajout du fichier « pytha.py » du Jupyter Notebook « test » dans le parcours « Intégrer l'algorithmique et la programmation au LP (Python)  ».

---
+++ ac-amiens/7496/pytha.py
@@ -0,0 +1,13 @@
+# Créé par za, le 06/11/2020 en Python 3.7
+
+from math import*
+#la bibliothèque lycee n'existe pas sur le notebook (en ligne)
+# nous vous conseillons de l'utiliser sur votre PC
+cote_1=15
+cote_2=20
+
+hyp_carre=cote_1**2+cote_2**2
+hyp=sqrt(hyp_carre)
+
+
+print ("l'hypoténuse du triangle rectangle vaut", hyp)
