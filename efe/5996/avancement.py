7ec9045f1072bf62ec1867fe2dc4d5a47907f7f3
gitmagistere <48481569+gitmagistere@users.noreply.github.com>
2021-09-19 15:40:07 +0200

    Ajout du fichier « avancement.py » du Jupyter Notebook « test 2 » dans le parcours « Bac à sable 1 formation Atelier M@gistère ».

---
+++ efe/5996/avancement.py
@@ -0,0 +1,21 @@
+﻿print("équation de réaction I2 + 2 S2O32 -> 2 I-  + + S4O62-")
+
+print("entrer les quantités de matières initiales")
+n0diiode=float(input ("n0diidode= (en mol) "))
+n0thio=float(input ("n0thiosulfate= (en mol)"))
+xmax1=n0diiode
+xmax2=n0thio/2
+if xmax1==xmax2:
+    print("les proportions sont stoechiométriques")
+    xmax=xmax1
+elif xmax1<xmax2:
+    print("le diiode est le réactif limitant")
+    xmax=xmax1
+else:
+    print("le thiosulfate  est le réactif limitant")
+    xmax=xmax2
+
+print("la valeur de l'avancement maximale est ",xmax, "mol")
+print("à l’état final, la quantité d’ions iodure est de ", 2*xmax, "mol\n")
+print("à l’état final, la quantité d’ions S4O62- est de ", xmax, "mol")
+
